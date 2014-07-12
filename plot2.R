4ea3eb64367932f41ce429ce321135323dbcda37
petehinchliffe <petehinchliffe@hotmail.com>
2014-07-12 08:36:01 +0100

    ExData Assignment 1: Doh! Use png format not pdf.

--- plot2.R
+++ plot2.R
@@ -4,6 +4,6 @@ hpc <- hpcAll[hpcAll$Date == '1/2/2007' | hpcAll$Date == '2/2/2007',]
 DTs <- strptime(paste(hpc$Date, hpc$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
 hpc <- cbind(DTs,hpc)
 
-pdf('plot2.pdf')
+png('plot2.png', width=480, height=480)
 with(hpc, plot(DTs, Global_active_power, type='l', main='', xlab='',ylab='Global Active Power (kilowatts)'))
 dev.off()
